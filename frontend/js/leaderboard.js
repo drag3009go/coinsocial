f6f6ee80f0db32a2fdee969585112d0b7990802c
drag3009go <alekseev.evgesha2108@gmail.com>
2026-04-13 17:32:31 +0900

    Update leaderboard.js

--- frontend/js/leaderboard.js
+++ frontend/js/leaderboard.js
@@ -12,31 +12,30 @@ class Leaderboard {
             console.error('Error loading leaderboard:', error);
         }
     }
-}
-    
 
     render(users) {
-    const container = document.getElementById('leaderboard');
-    container.innerHTML = `
-        <table class="leaderboard-table">
-            <thead>
-                <tr><th>#</th><th>Пользователь</th><th>Монеты</th><th>Титул</th></tr>
-            </thead>
-            <tbody>
-                ${users.map((user, index) => `
-                    <tr>
-                        <td>${index+1}</td>
-                        <td>
-                            <img src="${user.avatar_url ? API_BASE+user.avatar_url : 'default-avatar.png'}" class="avatar-small">
-                            ${user.username}
-                        </td>
-                        <td>${user.coins}</td>
-                        <td>${user.title}</td>
-                    </tr>
-                `).join('')}
-            </tbody>
-        </table>
-    `;
+        const container = document.getElementById('leaderboard');
+        container.innerHTML = `
+            <table class="leaderboard-table">
+                <thead>
+                    <tr><th>#</th><th>Пользователь</th><th>Монеты</th><th>Титул</th></tr>
+                </thead>
+                <tbody>
+                    ${users.map((user, index) => `
+                        <tr>
+                            <td>${index+1}</td>
+                            <td>
+                                <img src="${user.avatar_url ? API_BASE+user.avatar_url : 'default-avatar.png'}" class="avatar-small">
+                                ${user.username}
+                            </td>
+                            <td>${user.coins}</td>
+                            <td>${user.title}</td>
+                        </tr>
+                    `).join('')}
+                </tbody>
+            </table>
+        `;
+    }
 }
 
 const leaderboard = new Leaderboard();
