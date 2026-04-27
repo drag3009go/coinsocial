30fae8058a72c8e56a6b6e014078d6b11a9396eb
drag3009go <alekseev.evgesha2108@gmail.com>
2026-04-27 20:58:43 +0900

    Update messages.js

--- frontend/js/messages.js
+++ frontend/js/messages.js
@@ -2,7 +2,7 @@
 
 class MessageManager {
     constructor() {
-        this.initialized = true;
+        this.initialized = false;
         this.currentConversation = null;
         this.conversations = [];
         this.messages = [];
